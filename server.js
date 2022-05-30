4ef40e6e3ef895ca71c95d4e976c8ea9b7b37ba4
Rayyaan-1120 <rayyaancool@gmail.com>
2022-05-31 02:10:06 +0500

    boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
 const app = require('./App')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.DB_CONNECTION_STRING,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MONGODB CONNECTED')
+}).catch(err => console.log(err))
+
+app.use(cors('*'))
 
 
-app.listen(5000,() => {
+app.listen(PORT,() => {
     console.log('Server is running on port 5000')
 })
